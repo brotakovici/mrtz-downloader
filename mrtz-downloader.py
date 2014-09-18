91ec10ba5052b71d45841f31ed9769d488e36ea0
Andrei Maximilian Diamandopol <brotakovici@gmail.com>
2014-09-18 21:23:37 +0300

    Added title scraping

--- mrtz-downloader.py
+++ mrtz-downloader.py
@@ -1,5 +1,6 @@
-import requests
+import requests 
 from  parameterize import parameterize
+from bs4 import BeautifulSoup
 
 song = raw_input('Baga the song :')
 
@@ -8,5 +9,12 @@ song = parameterize(song)
 url = 'http://www.mrtzcmp3.net/' + song  + '_1s.html'
 
 res = requests.get(url)
+htmlContent = BeautifulSoup(res.content)
+spanList = htmlContent.find_all('span', {'class': 'mp3Title'})
+bitrateIndex = 0
 
-print res.text
+for span in spanList:
+    print span
+    bitrate = htmlContent.find('span', {'id' : 'bitrate_' + str(bitrateIndex)})
+    print bitrate
+    bitrateIndex = bitrateIndex + 1
